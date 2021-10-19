549009db65fbb5207de312961dffbb0c139aefe1
josuehyeon <feeshing7@gmail.com>
2021-10-19 15:12:26 +0900

    JESUS CHRIST

--- src/main/java/com/kh/project/stuReg/controller/StuRegController.java
+++ src/main/java/com/kh/project/stuReg/controller/StuRegController.java
@@ -1,12 +1,5 @@
 package com.kh.project.stuReg.controller;
 
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.kh.project.student.service.StudentService;
 
 public class StuRegController {
 	
