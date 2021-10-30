4ffe1127416d7a36b1bd50b628b9b6ac932df467
SujinKim <86155220+zet410@users.noreply.github.com>
2021-10-31 02:21:30 +0900

    1031_김수진_SearchVO추가

---
+++ src/main/java/com/kh/project/common/vo/SearchVO.java
@@ -0,0 +1,23 @@
+package com.kh.project.common.vo;
+
+public class SearchVO {
+	private String searchKeyword;
+	private String searchValue;
+	
+	
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
+	public String getSearchValue() {
+		return searchValue;
+	}
+	public void setSearchValue(String searchValue) {
+		this.searchValue = searchValue;
+	}
+	
+	
+
+}
